ce707778ffec32505e0cbfeb47468a064d74309d
Tatyana Scotce <tscotce@gmail.com>
2018-01-14 04:43:44 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,8 +34,10 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if valid_move?(board, index) == true
+  while valid_move?(board, index) == false
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
     return true
-  else puts "invalid"
   end 
 end
