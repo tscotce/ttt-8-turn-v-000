1f85b8d860f093d9d28ced72063d1d4e1ef97b02
Tatyana Scotce <tscotce@gmail.com>
2018-01-14 03:55:25 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,7 +7,7 @@ def display_board (board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
 end 
 
 def valid_move? (board, index)
-  if board[index] == " "
+  if board[index] == " " && index.between?(0, 8)
     return true 
   else 
     return false  
