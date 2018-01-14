609cf2c8d6667bf7630464964a77d3832106eb70
Tatyana Scotce <tscotce@gmail.com>
2018-01-14 03:40:29 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,3 +14,10 @@ def valid_move? (board, index)
   end
 end
 
+def position_taken?(board, index)
+  if board[index] == " " || board [index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
